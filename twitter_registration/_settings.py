f46c974fc647bd30fc016e58ccd498af516ab3b4
Ricardo Campos <ricardocamposp@gmail.com>
2015-10-02 02:37:41 +0000

    base settings

---
+++ twitter_registration/_settings.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+#PROD
+#CONSUMER_KEY = 'RYA6tJluBPa7INeicfFVyagLv'
+#CONSUMER_SECRET = '6Lb3FEndR7gLgU1aZugPZotwPsAjyVh5dQ7OHJjE5atUR2hmPA'
+#CALLBACK_URL = 'http://speak-app.co/register/callback'
+
+#DEV
+CONSUMER_KEY = '0xVCFb313OTqSk4JJ27ptY7eH'
+CONSUMER_SECRET = 'PgvpWwpTU4D3nOdBxxF9PYuCFhGCeOl7sPrlWyvgiPkQkf4CT6'
+CALLBACK_URL = 'http://127.0.0.1:8000/register/callback'
+TWITTER_ACCOUNT = '@speak-app'
+TEMPORARY_EMAIL_DOMAIN = 'speak-app.co'
+PASSWORD_SALT = 'salt and pepper :)'
